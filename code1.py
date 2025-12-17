61b38b9f134ff1ee5b4aa26b001aafef962b0eea
Nandita <nandita3035.beai25@chitkara.edu.in>
2025-12-17 21:32:29 +0530

    Code for function 1

---
+++ code1.py
@@ -0,0 +1,11 @@
+def count_number(num):
+    even=0
+    odd=0
+    for i in num:
+        if i%2==0:
+            even+=1
+        elif i%2!=0:
+            odd+=1
+    return (f"Even count:{even}, Odd count: {odd}")
+nums=eval(input("Enter list of numbers:"))
+print(count_number(nums))
